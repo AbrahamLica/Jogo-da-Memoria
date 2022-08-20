c0342c44e96520277943b3d066859b1a94025887
Abraham Licá <abrahamlica451@gmail.com>
2022-08-20 15:29:18 -0300

    finalizei o projeto

---
+++ imagens.js
@@ -0,0 +1,18 @@
+var arrayImagens = [
+  'img/1.png',
+  'img/2.png',
+  'img/3.png',
+  'img/4.png',
+  'img/5.png',
+  'img/6.png',
+  'img/7.png',
+  'img/8.png',
+  'img/1.png',
+  'img/2.png',
+  'img/3.png',
+  'img/4.png',
+  'img/5.png',
+  'img/6.png',
+  'img/7.png',
+  'img/8.png'
+]
---
+++ script.js
@@ -0,0 +1,123 @@
+////////////////////////////////////////////////////////////////////////////////
+
+var container = document.querySelector(".container")
+var cards = document.querySelectorAll('.card-img')
+var imgs = document.querySelectorAll(".img")
+var botao = document.querySelector(".jogar-dnv")
+var primeiroElemento = ''
+var segundoElemento = ''
+
+////////////////////////////////////////////////////////////////////////////////
+
+cards.forEach(function(i) {
+  addEventListener('click', revelaImagem)
+})
+
+////////////////////////////////////////////////////////////////////////////////
+
+function revelaImagem(e) {
+  var elemento = e.target
+
+  if (elemento.classList.contains('jogar-dnv')) {
+    reiniciarJogo()
+  } else if (primeiroElemento == '') {
+    if (elemento.children[0].classList.contains('img') == true) {
+      elemento.children[0].classList.remove('none')
+      elemento.children[0].classList.add('block')
+      primeiroElemento = elemento.children[0]
+    }
+  } else if (segundoElemento == '') {
+    if (elemento.classList.contains('img') == true) {
+      return
+    } else if (elemento.children[0].classList.contains('img') == true) {
+      elemento.children[0].classList.remove('none')
+      elemento.children[0].classList.add('block')
+      segundoElemento = elemento.children[0]
+
+      verificarPares()
+    }
+
+
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+function verificarPares() {
+
+  let primeiroElementoImagem = primeiroElemento.getAttribute('src')
+  let segundoElementoImagem = segundoElemento.getAttribute('src')
+
+  if (primeiroElementoImagem == segundoElementoImagem) {
+    primeiroElemento.parentNode.style.backgroundColor = 'green';
+    segundoElemento.parentNode.style.backgroundColor = 'green';
+    primeiroElemento = ''
+    segundoElemento = ''
+
+    verificarJogo()
+  } else {
+    primeiroElemento.parentNode.style.backgroundColor = 'red';
+    segundoElemento.parentNode.style.backgroundColor = 'red';
+    setTimeout(() => {
+      primeiroElemento.parentNode.style.backgroundColor = 'white';
+      segundoElemento.parentNode.style.backgroundColor = 'white';
+      primeiroElemento.classList.remove('block')
+      segundoElemento.classList.remove('block')
+      primeiroElemento = ''
+      segundoElemento = ''
+    }, 800);
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+function verificarJogo() {
+  var imagensSelecionadas = document.querySelectorAll(".block")
+  if (imagensSelecionadas.length === 16) {
+    container.style.display = 'none'
+
+    var parabens = document.querySelector(".container-parabens").cloneNode(true)
+    var addParabens = document.body.appendChild(parabens)
+    parabens.style.display = 'flex'
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function adicionaImagens() {
+  var arrayEmbaralhado = arrayImagens.sort(() => Math.random() - 0.5)
+  for (var i = 0; i < 16; i++) {
+    imgs[i].setAttribute('src', arrayEmbaralhado[i])
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+function reiniciarJogo() {
+  console.log('vamos reiniciar')
+  document.querySelector(".container-parabens").style.display = 'none'
+  container.style.display = 'flex'
+
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].style.display = 'block'
+    cards[i].style.backgroundColor = 'white'
+  }
+
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].classList.add('none')
+    imgs[i].classList.remove('block')
+  }
+
+  adicionaImagens()
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+window.addEventListener('load', adicionaImagens)
